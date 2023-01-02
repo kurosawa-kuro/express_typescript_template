665edf33df77d22e0c88be801cd08c9c1cc9f056
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-01-02 19:27:38 +0900

    asyncHandler

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
 import express, { Request, Response } from 'express';
 
 import setupLibrary from "./setupLibrary";
+import asyncHandler from './utils/asyncHandler';
 
 const app: express.Express = express();
 
 setupLibrary(app);
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', asyncHandler(async (req: Request, res: Response) => {
     res.json({ message: "Hello World" });
-});
+}));
 
 export default app;
