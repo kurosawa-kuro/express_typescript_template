9885c8a78e531dd41de9e7625ce3ab62fceefb5a
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-01-02 15:38:30 +0900

    setupLibrary

--- __tests__/routes/index.routes.test.ts
+++ __tests__/routes/index.routes.test.ts
@@ -5,7 +5,7 @@ import app from "../../src/app";
 describe("Test app.ts", () => {
     test("index route", async () => {
         const res = await request(app).get("/");
-        console.log({ res })
+        console.log('res.body', res.body)
         expect(res.body).toEqual({ message: "Hello World" });
     });
 });
