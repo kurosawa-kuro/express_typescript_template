2ecf3f2f7667a09e411e5efa0588e87bcfe418ef
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-01-02 14:35:11 +0900

    express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('modemon index')
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors({
+    credentials: true,
+    origin: ["http://localhost:3000"]
+}));
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World');
+});
+
+app.listen(8000, () => {
+    console.log('listening to port 8000')
+});
