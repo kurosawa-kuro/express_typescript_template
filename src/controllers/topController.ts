292dbb2955ee73312d8f9a4a2559dc2fa5ccd65c
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-01-02 20:03:53 +0900

    services

--- src/controllers/topController.ts
+++ src/controllers/topController.ts
@@ -2,8 +2,10 @@
 import { Request, Response } from "express";
 
 import asyncHandler from '../utils/asyncHandler';
+import { indexService } from '../services/topService';
 
 export const indexAction = asyncHandler(async (req: Request, res: Response) => {
+    const data = await indexService()
 
-    return res.status(200).json({ message: "Hello World" });
+    return res.status(200).json(data);
 });
