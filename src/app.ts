c5febd89a287652c0c328dc6dde911d2d0c19397
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-01-02 15:04:27 +0900

    swagger

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,21 @@
 import express, { Request, Response } from 'express';
 import logger from "morgan";
 import cors from 'cors';
+import swaggerUi from "swagger-ui-express";
+import swaggerJSDoc from "swagger-jsdoc";
 
 const app = express();
+
+const options = {
+    swaggerDefinition: {
+        info: {
+            title: "Express TypeScript",
+            version: "1.0.0"
+        }
+    },
+    apis: ["routes/*"]
+};
+app.use("/spec", swaggerUi.serve, swaggerUi.setup(swaggerJSDoc(options)));
 
 app.use(logger("dev"));
 
